3da468151647b6e502eaa03d996dc7647a77a679
Maciej Barć <xgqt@protonmail.com>
2020-09-23 10:41:34 +0200

    add more page stuff

--- gen-pkgs.sh
+++ gen-pkgs.sh
@@ -19,6 +19,44 @@ cd src_prepare-overlay || exit 1
 mkdir -p "${public_dir}"
 rm "${public_dir}/index.html"
 
+cat >> "${public_dir}/index.html" << BLOCK
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="generator" content="GitLab Pages" />
+    <meta name="description" content="src_prepare" />
+    <meta name="keywords" content="src_prepare" />
+    <meta name="author" content="src_prepare" />
+    <title>Gen-PKGs (src_prepare)</title>
+</head>
+<body>
+<h1>
+    Package report generated each day at 01:00 AM (or otherwise by Gitlab pipeline schedule)
+</h1>
+<ul>
+    <li>
+        https://gitlab.com/src_prepare/gen-pkgs
+    </li>
+    <li>
+        https://gitlab.com/src_prepare/gen-pkgs/-/jobs
+    </li>
+    <li>
+        https://gitlab.com/src_prepare/gen-pkgs/-/pipeline_schedules
+    </li>
+</ul>
+<p>
+    Curent project status:
+    <img src="https://gitlab.com/src_prepare/gen-pkgs/badges/master/pipeline.svg">
+    <img src="https://gitlab.com/src_prepare/badge/-/raw/master/feed-atom-orange.svg">
+<p>
+<p>
+    For a GPO report go to: https://gpo.zugaina.org/Overlays/src_prepare-overlay
+</p>
+BLOCK
+
+
 for meta in $(find . -name metadata.xml | sort)
 do
     pkg="$(dirname "${meta}" | sed 's/.\///')"
@@ -50,6 +88,8 @@ cat >> "${public_dir}/index.html" << BLOCK
 <p>
     Generated on: $(date)
 </p>
+</body>
+</html>
 BLOCK
 
 cd "${here_dir}" || exit 1
